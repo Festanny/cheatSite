5660e676635de08515864853c8e37ded7dd6555e
Festanny <60522731+Festanny@users.noreply.github.com>
2022-09-23 11:59:26 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -81,30 +81,32 @@ $('.sliderCustomizeService .swiper-slide .function input').on('input', function(
 });
 
 // allSumOrder
-$('.servicesBlock .block').on('click', function () {
-  var id = $(this).attr('data-tab')
-  tabEdit(id)
+$('.servicesBlock .block').on('click change', function () {
+  var id = $(this).attr('data-tab'),
+      commission = Number($('.itemTabServices  .block[data-tab="' + id + '"] .paymentMain select option:selected').attr('data-com-sys')) / 100;
+  tabEdit(id, commission)
 });
 
 // TabOrder
 $('.servicesBlock .swiper-slide.slideBlock').click(function () {
 	var id = $(this).attr('data-tab'),
-  content = $('.servicesBlock .block[data-tab="' + id + '"]');
+      content = $('.servicesBlock .block[data-tab="' + id + '"]'),
+      commission = Number($('.itemTabServices .block[data-tab="' + id + '"] .paymentMain select option:selected').attr('data-com-sys')) / 100;
 
 	$('.servicesBlock .swiper-slide.slideBlock.active').removeClass('active');
 	$(this).addClass('active');
 
 	$('.servicesBlock .block.active').removeClass('active');
 	content.addClass('active');
-  tabEdit(id);
+  tabEdit(id, commission);
 });
 
 // tabEdit
-function tabEdit(id) {
+function tabEdit(id, commission) {
   var contentCount = $('.servicesBlock .block[data-tab="' + id + '"] input[type="range"]').length,
       sumComm = 0,
-      sumAllOrder = 0,
-      commission = 0.03
+      sumAllOrder = 0;
+  $('form .payment .commission').text((commission * 100).toFixed(1));
   for (var i=0; i<contentCount; i++) {
     sumComm = sumComm + Number($('.servicesBlock .block[data-tab="' + id + '"] input[type="range"]').eq(i).next().children().text()) * commission;
     sumAllOrder = sumAllOrder + Number($('.servicesBlock .block[data-tab="' + id + '"] input[type="range"]').eq(i).next().children().text());
